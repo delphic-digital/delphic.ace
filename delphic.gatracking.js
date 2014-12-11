e8acd974266ca8171d62b52c1987f654aa62e36e
Jeff <jeffy9@gmail.com>
2014-12-11 12:12:51 -0500

    Check for previous bound click events and handle them gracefully

--- delphic.gatracking.js
+++ delphic.gatracking.js
@@ -2,33 +2,58 @@
 
 ;(function(DELPHIC, $) {
 
-	$.fn.trackMe = function(opts) {
+	var debug = {};
+	debug.log = function(){};
+	if (window.console && window.console.log)
+		debug.log = function(msg) { window.console.log(msg); };
 
-		$(this).click(function(e){
-			e.preventDefault();
+	$.fn.getEvents = function() {
+		if (typeof($._data) == 'function') {
+			return $._data(this.get(0), 'events') || {};
+		} else if (typeof(this.data) == 'function') { // jQuery version < 1.7.?
+			return this.data('events') || {};
+		}
+		return {};
+	};
 
+
+	$.fn.trackMe = function() {
+
+		var $this = $(this),
+		    preventDefaultClick;
+
+		//Check for already assigned click events.
+		clickEvents = $this.getEvents().click;
+
+		$this.click(function(e){
+			if(!clickEvents)
+				e.preventDefault();
 			var $this = $(this),
 			    href = $this.attr('href'),
 			    target = $this.attr('target'),
-			    category = $this.attr('track-event'),
-			    action = $this.attr('track-action'),
-			    label = $this.attr('track-label'),
-			    value = $this.attr('track-value'),
-			    nonInteraction = $this.attr('track-nonInteraction');
+			    category = $this.data('track-event'),
+			    action = $this.data('track-action'),
+			    label = $this.data('track-label'),
+			    value = $this.data('track-value'),
+			    nonInteraction = $this.data('track-nonInteraction');
 
 			action = action?action:'Click';
 			label = label?label:window.location.pathname;
 
+			debug.log(">>>>>>>>>>> " + "'send','event',"+ category+","+ action+","+ label+","+ value+","+ nonInteraction);
 			ga('send', 'event', category, action, label, value, nonInteraction);
 
-			if(href){
-				setTimeout(function() {
-					if(target == '_blank'){
-						window.open(href);
-					}else{
-						document.location.href = href;
-					}
-				}, 80);
+
+			if(!clickEvents){
+				if(href){
+					setTimeout(function() {
+						if(target == '_blank'){
+							window.open(href);
+						}else{
+							document.location.href = href;
+						}
+					}, 80)
+				}
 			}
 
 		});
